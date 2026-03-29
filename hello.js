e2e6d9e0cf0b39bd08e27b149ee5d461e985336e
Minh <minh.vut@one-line.com>
2026-03-29 19:56:35 +0700

    Add sayGoodbye function

--- hello.js
+++ hello.js
@@ -1,5 +1,10 @@
 function sayHello(name) {
-  return `Hello, ${name}!`;
+  return "Hello, " + name + "!";
+}
+
+function sayGoodbye(name) {
+  return "Goodbye, " + name + "!";
 }
 
 console.log(sayHello("Automation Tester"));
+console.log(sayGoodbye("Manual Testing"));
