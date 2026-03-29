1b97acd5629668e4915d7c10603363c7b0894093
Minh <minh.vut@one-line.com>
2026-03-29 20:02:59 +0700

    Add sayFarewell function

--- hello.js
+++ hello.js
@@ -10,6 +10,11 @@ function sayWelcome(name) {
   return "Welcome, " + name + "!";
 }
 
+function sayFarewell(name) {
+  return "Farewell, " + name + "!";
+}
+
 console.log(sayHello("Automation Tester"));
 console.log(sayGoodbye("Manual Testing"));
 console.log(sayWelcome("New User"));
+console.log(sayFarewell("Old User"));
