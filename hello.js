ea6eda247e16705799effe43324cef2e26218abf
Minh <minh.vut@one-line.com>
2026-03-29 20:01:48 +0700

    Add sayWelcome function

--- hello.js
+++ hello.js
@@ -6,5 +6,10 @@ function sayGoodbye(name) {
   return "Goodbye, " + name + "!";
 }
 
+function sayWelcome(name) {
+  return "Welcome, " + name + "!";
+}
+
 console.log(sayHello("Automation Tester"));
 console.log(sayGoodbye("Manual Testing"));
+console.log(sayWelcome("New User"));
